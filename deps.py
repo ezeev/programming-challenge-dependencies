9cf0c1fbe89f3a29bea6ac31cf188df2c9a93fca
ezeev <evan.pease@gmail.com>
2022-12-16 06:35:35 -0800

    first pass before proper testing

---
+++ deps.py
@@ -0,0 +1,48 @@
+
+import sys
+
+
+all_deps = []
+depths = []
+def parse_deps(name: str, depth: int):
+
+    all_deps.append(name)
+    depths.append(depth)
+    prefix = 'apps/' + name
+    if name.startswith('lib'):
+         prefix = 'libs/' + name[3:len(name)]
+    #print(f'opening {prefix}/meta.txt')
+    f = open(f'{prefix}/meta.txt', "r")
+    for line in f:
+        l = line
+        if (line.startswith("deps:")):
+            deps_str = line.split(":")[1]
+            new_deps = [d.strip() for d in deps_str.split(",")]
+            for dep in new_deps:
+                #print(type(deps))
+                if dep in all_deps:
+                    continue
+                parse_deps(dep, depth+1)
+
+
+    #return ([d.strip() for d in deps])
+
+
+
+def main(app: str):
+    print(f'Getting dependencies for {app}')
+    parse_deps(app, 0)
+    for i, dep in enumerate(all_deps):
+        tabs = ""
+        for x in range(depths[i]):
+            tabs+="+"
+        print(tabs+dep)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("You must pass in the name of the app to scan for dependencies! Example:")
+        print("\tpython3 deps.py breakfast")
+        exit()
+    app = sys.argv[1]
+    main(app)
